3081b1ab046adaa35b782430fb94a0c2dfec8ad0
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-03-12 11:58:32 +0800

    Add Interfaces project

---
+++ CharacterRecognitionBP.Interfaces/IWaiter.cs
@@ -0,0 +1,7 @@
+﻿namespace CharacterRecognitionBP.Interfaces
+{
+    public interface IWaiter
+    {
+        ValueTask<bool> ForTrueAsync(Func<bool> predicate, int timeout, int sleepOverride = -1, CancellationToken token = default);
+    }
+}
