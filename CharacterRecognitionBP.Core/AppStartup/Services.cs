a67c1d42577944c30882ad655275a6a115319a3e
Sly Kint A. Bacalso <slykint.bacalso@cit.edu>
2023-03-12 11:57:50 +0800

    Add Core project

---
+++ CharacterRecognitionBP.Core/AppStartup/Services.cs
@@ -0,0 +1,16 @@
+﻿using CharacterRecognitionBP.Core.Interfaces;
+using CharacterRecognitionBP.Interfaces;
+using Microsoft.Extensions.DependencyInjection;
+
+namespace CharacterRecognitionBP.Core.AppStartup
+{
+    public static class Services
+    {
+        public static IServiceCollection AddInterfaces(this IServiceCollection services)
+        {
+            services.AddSingleton<IWaiter, Waiter>();
+
+            return services;
+        }
+    }
+}
---
+++ CharacterRecognitionBP.Core/Interfaces/Waiter.cs
@@ -0,0 +1,31 @@
+﻿using CharacterRecognitionBP.Interfaces;
+
+namespace CharacterRecognitionBP.Core.Interfaces
+{
+    public class Waiter : IWaiter
+    {
+        public async ValueTask<bool> ForTrueAsync(Func<bool> predicate, int timeout, int sleepOverride = -1, CancellationToken token = default)
+        {
+            return await ForTrueAsync(predicate, null, timeout, sleepOverride, token);
+        }
+
+        private async ValueTask<bool> ForTrueAsync(Func<bool> predicate, Action? loopFunction, int timeout, int sleepOverride = -1, CancellationToken token = default)
+        {
+            try
+            {
+                int counter = 0;
+                while (!predicate() && !token.IsCancellationRequested)
+                {
+                    if (timeout > 0 && counter >= timeout)
+                        return false;
+                    loopFunction?.Invoke();
+                    await Task.Delay(sleepOverride == -1 ? 20 : sleepOverride, token);
+                    counter++;
+                }
+                return true;
+            }
+            catch { }
+            return false;
+        }
+    }
+}
